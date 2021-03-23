fd0ed5967875bf0ccfc5414d27d05d70265cb8b8
EasySocHacks <easy.soc.hacks@gmail.com>
2021-03-23 21:49:57 +0000

    Tried to add logic of shifting calendar for 12 month period (Fail). At German's request, added some already written code connected with it (There is a huge problem with getting a basic handler). And finnaly removed calendar header from page.

--- webapp/controller/Calendar.controller.js
+++ webapp/controller/Calendar.controller.js
@@ -1,9 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller", 
-    'sap/ui/model/json/JSONModel',
     'sap/ui/core/format/DateFormat',
     'sap/ui/core/library'
-], function (Controller, JSONModel, DateFormat, coreLibrary) {
+], function (Controller, DateFormat, coreLibrary) {
     "use strict";
 
     var CalendarType = coreLibrary.CalendarType;
@@ -17,10 +16,31 @@ sap.ui.define([
             this.currentYear = this.oFormatYyyy.format(new Date());
 
             var calendar = this.byId("calendar");
-            var startDate = new Date(this.currentYear, 0, 1)
             
             calendar.displayDate(new Date(this.currentYear, 0, 1));
-        }
+        }/*,
+
+        onAfterRendering: function() {
+            var calendar = this.byId("calendar");
+            var header = calendar.getAggregation("header");
+
+            //Somehow doesn't work
+            header.setVisibleButton1(false);
+
+            header.attachPressNext(function(oEvent) {
+                //mEventRegistry.press = undefined
+                //The problem is to get an existing handler from a new one or
+                //to get it from an external source => this way we need a link to the button.
+                //Instead this we got just a functions to manipulate with this buttons...
+                //
+                //This code allows us to detach current attached function, despite this we still have no info,
+                // how to get an initial one...
+                header.detachPressNext(oEvent.getSource().mEventRegistry.press[0].fFunction);
+
+                //Here we would be detach current handler and attach a new handler to shift 12 months:
+                //Increment(Decrement to previous button) this.currentYear and do calendar.displayDate.
+            });
+        }*/
     });
 
 });
